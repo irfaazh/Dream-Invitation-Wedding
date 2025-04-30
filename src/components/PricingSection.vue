2db20590015b59081144b290c3587acd81716bb7
unknown <irfazhafir0@gmail.com>
2025-04-30 13:58:38 +0700

    Update konten

--- src/components/PricingSection.vue
+++ src/components/PricingSection.vue
@@ -1,113 +1,90 @@
 <template>
   <section id="pricing" class="pb-8">
     <v-container fluid>
-      <v-row align="center" justify="center">
-        <v-col cols="10">
-          <v-card style="width: 100%">
-            <h1 class="pt-6 font-weight-light dark-slate-blue-text display-2 text-center">Harga Katalog</h1>
-            <v-divider class="my-6"></v-divider>
-            <v-row>
-              <!-- Paket 1 -->
-              <v-col cols="12" sm="6" md="4">
-                <div class="flex-center">
-                  <v-card-text>
-                    <div class="text--disabled text-uppercase text-h5 my-2 text-center">Simple Non photo</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Dengan fitur lengkap</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Belum termasuk domain custom (+Rp 30.000,00 untuk domain .my.id)</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Palet warna yang beragam (client bisa milih tema warna)</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase text-h4 mt-6 blue--text text-center">Rp 120.000,00</div>
-                    <div class="text-center">
-                      <v-btn
-                        rounded outlined color="blue" class="mt-6" href="https://sample-polos-diw.vercel.app" target="_blank"
-                        rel="noopener"
-                      >
-                        Lihat Sample
-                      </v-btn>
-                    </div>
-                  </v-card-text>
-                  <v-divider style="margin-right: -23px" vertical v-if="$vuetify.breakpoint.smAndUp"></v-divider>
-                </div>
-                <v-divider class="mx-4" v-if="!$vuetify.breakpoint.smAndUp"></v-divider>
-              </v-col>
+      <h1 class="pt-6 font-weight-light dark-slate-blue-text display-2 text-center">Harga Katalog</h1>
+      <v-divider class="my-6"></v-divider>
 
-              <!-- Paket 2 -->
-              <v-col cols="12" sm="6" md="4">
-                <div class="flex-center">
-                  <v-card-text>
-                    <div class="text--disabled text-uppercase text-h5 my-2 text-center">Simple with photo</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Dengan 7 foto</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Dengan fitur lengkap</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Belum termasuk domain custom (+Rp 30.000,00 untuk domain .my.id)</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Palet warna yang beragam (client bisa milih tema warna)</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase text-h4 mt-6 blue--text text-center">Rp 130.000,00</div>
-                    <div class="text-center">
-                      <v-btn
-                        rounded outlined color="blue" class="mt-6" href="https://sample-foto-diw.vercel.app" target="_blank"
-                        rel="noopener"
-                      >
-                        Lihat Sample
-                      </v-btn>
-                    </div>
-                  </v-card-text>
-                  <v-divider style="margin-right: -23px" vertical v-if="$vuetify.breakpoint.mdAndUp"></v-divider>
-                </div>
-                <v-divider class="mx-4" v-if="!$vuetify.breakpoint.smAndUp"></v-divider>
-              </v-col>
+      <v-row dense>
+        <!-- Paket 1 -->
+        <v-col cols="12" sm="6" md="4">
+          <v-card outlined class="pa-2" max-width="320px" style="margin: 0 auto;">
+            <v-img :src="require('@/assets/img/mockup-spl-polos.png')" height="200px" contain></v-img>
+            <v-card-actions class="justify-start px-4 pt-4 pb-0">
+              <v-btn block outlined color="blue" href="https://sample-polos-diw.vercel.app" target="_blank" rel="noopener">
+                Preview
+              </v-btn>
+            </v-card-actions>
 
-              <!-- Paket 3 -->
-              <v-col cols="12" md="4">
-                <v-divider v-if="$vuetify.breakpoint.smOnly" class="mx-4"></v-divider>
-                <div class="flex-center">
-                  <v-card-text>
-                    <div class="text--disabled text-uppercase text-h5 my-2 text-center">Request Referensi</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Menyesuaikan foto dan tata letak</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Fitur menyesuaikan sesuai request</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Belum termasuk domain custom (+Rp 30.000,00 untuk domain .my.id)</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase blue--text text-left">Palet warna/logo boleh di sediakan oleh client atau dibuatkan oleh kami</div>
-                    <v-divider class="my-2" />
-                    <div class="text-uppercase text-h4 mt-6 blue--text text-center">Rp 110.000,00 s/d Rp 150.000,00</div>
-                    <div class="text-center">
-                      <v-btn
-                        rounded outlined color="blue" class="mt-6" href="https://www.instagram.com/diw.project" target="_blank"
-                        rel="noopener"
-                      >
-                        Konsultasi Sekarang
-                      </v-btn>
-                    </div>
-                  </v-card-text>
-                </div>
-              </v-col>
+            <v-card-text class="text-left">
+              <div class="text--disabled text-uppercase text-h6 font-weight-bold mb-2">Simple Non Photo</div>
+              <div class="blue--text mb-1">Dengan fitur lengkap</div>
+              <div class="blue--text mb-1">Belum termasuk domain custom (+Rp 30.000,00 untuk domain .my.id)</div>
+              <div class="blue--text mb-1">Palet warna yang beragam (client bisa milih tema warna)</div>
+              <div class="text-h6 mt-4 blue--text font-weight-bold">Rp 120.000,00</div>
+            </v-card-text>
 
-            </v-row>
+            <v-card-actions class="px-4 pt-0 pb-4">
+              <v-btn block color="blue" dark href="https://www.instagram.com/diw.project" target="_blank">
+                Order
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <!-- Paket 2 -->
+        <v-col cols="12" sm="6" md="4">
+          <v-card outlined class="pa-2" max-width="320px" style="margin: 0 auto;">
+            <v-img :src="require('@/assets/img/mockup-spl-foto.png')" height="200px" contain></v-img>
+            <v-card-actions class="justify-start px-4 pt-4 pb-0">
+              <v-btn block outlined color="blue" href="https://sample-foto-diw.vercel.app" target="_blank" rel="noopener">
+                Preview
+              </v-btn>
+            </v-card-actions>
+
+            <v-card-text class="text-left">
+              <div class="text--disabled text-uppercase text-h6 font-weight-bold mb-2">Simple with Photo</div>
+              <div class="blue--text mb-1">Dengan 7 foto</div>
+              <div class="blue--text mb-1">Dengan fitur lengkap</div>
+              <div class="blue--text mb-1">Belum termasuk domain custom (+Rp 30.000,00 untuk domain .my.id)</div>
+              <div class="blue--text mb-1">Palet warna yang beragam (client bisa milih tema warna)</div>
+              <div class="text-h6 mt-4 blue--text font-weight-bold">Rp 130.000,00</div>
+            </v-card-text>
+
+            <v-card-actions class="px-4 pt-0 pb-4">
+              <v-btn block color="blue" dark href="https://www.instagram.com/diw.project" target="_blank">
+                Order
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <!-- Paket 3 -->
+        <v-col cols="12" sm="6" md="4">
+          <v-card outlined class="pa-2" max-width="320px" style="margin: 0 auto;">
+            <v-img :src="require('@/assets/img/reload.png')" height="200px" contain></v-img>
+            <v-card-actions class="justify-start px-4 pt-4 pb-0">
+              <v-btn block outlined color="blue" href="https://www.instagram.com/diw.project" target="_blank" rel="noopener">
+                Preview
+              </v-btn>
+            </v-card-actions>
+
+            <v-card-text class="text-left">
+              <div class="text--disabled text-uppercase text-h6 font-weight-bold mb-2">Request Referensi</div>
+              <div class="blue--text mb-1">Menyesuaikan foto dan tata letak</div>
+              <div class="blue--text mb-1">Fitur menyesuaikan sesuai request</div>
+              <div class="blue--text mb-1">Belum termasuk domain custom (+Rp 30.000,00 untuk domain .my.id)</div>
+              <div class="blue--text mb-1">Palet warna/logo boleh disediakan oleh client atau dibuatkan oleh kami</div>
+              <div class="text-h6 mt-4 blue--text font-weight-bold">Rp 110.000,00 s/d Rp 150.000,00</div>
+            </v-card-text>
+
+            <v-card-actions class="px-4 pt-0 pb-4">
+              <v-btn block color="blue" dark href="https://wa.me/6281234567890?text=Order%20Request%20Referensi" target="_blank">
+                Order
+              </v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
     </v-container>
-
-    <div class="svg-border-rounded text-light">
-      <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-          preserveAspectRatio="none"
-          fill="currentColor"
-      >
-        <path
-            d="M0,64L80,90.7C160,117,320,171,480,181.3C640,192,800,160,960,138.7C1120,117,1280,107,1360,101.3L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-        ></path>
-      </svg>
-    </div>
   </section>
 </template>
